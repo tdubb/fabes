6b10e37847290a5cde66e51e244b4d9c3cf9fd6d
Tyler Wasden <twasden@gmail.com>
2013-04-04 07:19:19 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabes::Application.config.secret_token = 'e5075628f36009f561f003bb03c43a32e0eedeb8b7fb9deaff91b9c6f547147cd8bdfa2d60706016c928b437e5474f25a369932e6638071d28913874fcfdf28c'
